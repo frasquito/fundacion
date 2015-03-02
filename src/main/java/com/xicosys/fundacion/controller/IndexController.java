8d6a2f4969b806b067b03a566faf0c0c3f8bbdf9
frasquito <arieltowers@gmail.com>
2015-03-02 01:53:47 -0300

    segundo commit

---
+++ src/main/java/com/xicosys/fundacion/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.xicosys.fundacion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+
+}
